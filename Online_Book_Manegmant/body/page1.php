32becafa0e425e67dd0e1cc8ea30f74a3d6a5726
Jitchangdarr <jitchangdar7@gmail.com>
2025-01-01 15:34:41 +0530

    some Folder Add

---
+++ Online_Book_Manegmant/body/page1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/body/page1.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/home/index.php">Home</a></li>
+                <li><a href="categories.html">Categories</a></li>
+                <li><a href="/signup/page1.php">Sign Up</a></li>
+                <li><a href="login.html">Login</a></li>
+                <li><a href="/contact/contactus.php">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <div class="loader">
+            <div class="image">
+                <img src="https://oxfordbookstore.com/public/uploads/home_content_images/1667369734_4.png" alt="">
+                <h4>New Arrivals</h4>
+            </div>
+            <div class="image">
+                <img src="https://oxfordbookstore.com/public/uploads/home_content_images/1667372109_2.png" alt="">
+                <h4>Best Sellers</h4>
+            </div>
+            <div class="image">
+                <img src="https://oxfordbookstore.com/public/uploads/home_content_images/1667372109_3.png" alt="">
+                <h4>Top Rated</h4>
+            </div>
+            <div class="image">
+                <img src="https://oxfordbookstore.com/public/uploads/home_content_images/1685516372_0.png" alt="">
+                <h4>Featured</h4>
+            </div>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
