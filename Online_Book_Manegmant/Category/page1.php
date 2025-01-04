8aaa3a56e70735b228873d3db700baa97d6d24a8
Jitchangdarr <jitchangdar7@gmail.com>
2025-01-04 23:57:05 +0530

    add css style

--- Online_Book_Manegmant/Category/page1.php
+++ Online_Book_Manegmant/Category/page1.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="page1.css">
 </head>
+
 <body>
-    
+    <nav>
+        <div class="categories">
+            <ul>
+                <li><a href="#">Fiction</a></li>
+                <li><a href="#">Non-Fiction</a></li>
+                <li><a href="#">Children</a></li>
+                <li><a href="#">Romance</a></li>
+                <li><a href="#">Mystery</a></li>
+                <li><a href="#">Horror</a></li>
+                <li><a href="#">Thriller</a></li>
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Adventure</a></li>
+                <li><a href="#">Comedy</a></li>
+                <li><a href="#">Drama</a></li>
+                <li><a href="#">Fantasy</a></li>
+                <li><a href="#">Historical</a></li>
+                <li><a href="#">Magical Realism</a></li>
+                <li><a href="#">Poetry</a></li>
+            </ul>
+        </div>
+    </nav>
 </body>
+
 </html>
